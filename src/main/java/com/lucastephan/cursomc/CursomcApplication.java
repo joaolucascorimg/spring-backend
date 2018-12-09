9dfa52b3f43819bc001b82ec11b14f7c086310ac
lucastephan <lucastephann@gmail.com>
2018-12-08 23:17:39 -0200

    Criação dos produtos
    Foi criada a classe de domínio Produto, e todas as classes relacionadas (conforme foi feito com a Categoria). Feito também um relacionamento (N:M) entre Produto e Categoria.

--- src/main/java/com/lucastephan/cursomc/CursomcApplication.java
+++ src/main/java/com/lucastephan/cursomc/CursomcApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.lucastephan.cursomc.domain.Categoria;
+import com.lucastephan.cursomc.domain.Produto;
 import com.lucastephan.cursomc.repositories.CategoriaRepository;
+import com.lucastephan.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -25,6 +29,20 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		// Associacoes de produtos em categorias
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		// Associacoes de categorias em produtos
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
