590b2fbcab23c3f3df1f3fe53e80d98decd9fa0f
lucastephan <lucastephann@gmail.com>
2018-12-22 19:37:10 -0200

    GET, PUT e DELETE para a classe Cliente
    Sempre manter um padrão de nomes para aproveitar os métodos em novas classes (utilizados os métodos da classe de CategoriaService).
    
    Operador ternário no construtor do Cliente para tratar NullPointerException (CPF/CNPJ não pode mudar com PUT).
    
    Método "updateData" (ClienteService) tem como finalidade atualizar somente os dados que foram classificados como mutáveis no PUT (nome e email).

--- src/main/java/com/lucastephan/cursomc/services/ClienteService.java
+++ src/main/java/com/lucastephan/cursomc/services/ClienteService.java
@@ -1,14 +1,20 @@
 package com.lucastephan.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.lucastephan.cursomc.domain.Cliente;
+import com.lucastephan.cursomc.dto.ClienteDTO;
 import com.lucastephan.cursomc.repositories.ClienteRepository;
-
-import com.lucastephan.cursomc.services.exceptions.*;
+import com.lucastephan.cursomc.services.exceptions.DataIntegrityException;
+import com.lucastephan.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
@@ -21,6 +27,40 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto nao encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()
 		));
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = this.find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		this.find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao e possivel excluir um cliente com pedidos atribuidos.");
+		}
+	}
+	
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDto(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 
 }
