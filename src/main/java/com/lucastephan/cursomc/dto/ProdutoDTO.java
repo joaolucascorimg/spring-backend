f549a1ea18854f8f97669837d31378f9758f2b0f
lucastephan <lucastephann@gmail.com>
2018-12-26 17:53:14 -0200

    Busca de produtos por nome e categorias
    Utilizando JPQL

---
+++ src/main/java/com/lucastephan/cursomc/dto/ProdutoDTO.java
@@ -0,0 +1,44 @@
+package com.lucastephan.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.lucastephan.cursomc.domain.Produto;
+
+public class ProdutoDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+	private Double preco;
+	
+	// Constructor
+	public ProdutoDTO() {
+	}
+	
+	public ProdutoDTO(Produto obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.preco = obj.getPreco();
+	}
+
+	// Getters & Setters
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+}
