e5e1865b0027519a366e70aac30141d68dbb127a
lucastephan <lucastephann@gmail.com>
2019-01-07 23:50:38 -0200

    Implementando autorização

--- src/main/java/com/lucastephan/springbackend/security/JWTUtil.java
+++ src/main/java/com/lucastephan/springbackend/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -20,11 +21,37 @@ public class JWTUtil {
 	// Metodo gerador do JWT Token
 	public String generateToken(String email) {
 		return Jwts.builder().setSubject(email) // Pega o username
-				.setExpiration(new Date(System.currentTimeMillis() + expiration)) // Data atual + tempo de expiracao em
-																					// milissegundos
-				.signWith(SignatureAlgorithm.HS512, secret.getBytes()) // Assinatura escolhendo o algoritimo e
-																		// transpilacao do segredo de string para bytes
+				.setExpiration(new Date(System.currentTimeMillis() + expiration)) // Data atual + tempo de expiracao em milissegundos
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes()) // Assinatura escolhendo o algoritimo e transpilacao do segredo de string para bytes
 				.compact(); // Compactacao do JWT String
+	}
+
+	public boolean tokenValido(String token) {
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			String username = claims.getSubject(); // Funcao do claims para retornar o usuario
+			Date expirationDate = claims.getExpiration(); // Funcao do claims para retornar a data de expiracao
+			Date now = new Date(System.currentTimeMillis());
+			if (username != null && expirationDate != null && now.before(expirationDate))
+				return true;
+		}
+		return false;
+	}
+	
+	public String getUsername(String token ) {
+		Claims claims = getClaims(token);
+		if (claims != null)
+			return claims.getSubject();
+		return null;
+	}
+
+	// Metodo para obter os Claims a partir de um token
+	private Claims getClaims(String token) {
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 }
